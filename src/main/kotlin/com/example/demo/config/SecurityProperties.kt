d2b7cc31b2dfe080d41acb525459d39c301968e8
Fabio Delgado <fabio.delgado2@gmail.com>
2021-05-17 22:53:25 -0300

    correção token jwt parametro

--- src/main/kotlin/com/example/demo/config/SecurityProperties.kt
+++ src/main/kotlin/com/example/demo/config/SecurityProperties.kt
@@ -1,14 +1,25 @@
 package com.example.demo.config
 
 import org.hibernate.validator.constraints.Length
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "jwt-security")
 class SecurityProperties {
+
     @Length(min = 42, message = "Minimum length for the secret is 42.")
-    var secret = "g6IJFdFhSBy8UwPhYaZ9l10XMl0XBwCisKrynxU7zI5J5KrTlHTM9JkMwbjxMmU5yDtb1E"
-    var expirationTime: Int = 31 // in days
-    var tokenPrefix = "Bearer "
-    var headerString = "Authorization"
-    var strength = 10
+    @Value("\${jwt.secret}")
+    lateinit var secret: String
+
+    @Value("\${jwt.expirationTime}")
+    val  expirationTime: Int = 31  // in days
+
+    @Value("\${jwt.tokenPrefix}")
+    lateinit var tokenPrefix  : String
+
+    @Value("\${jwt.headerString}")
+    lateinit var headerString : String
+
+    @Value("\${jwt.strength}")
+    val strength : Int = 10
 }
